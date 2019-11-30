899098112b514283264f98cd1daa0e26fc097846
Thanhhv317 <thanhhoang317@gmail.com>
2019-11-30 09:01:38 +0700

    create model ket qua

--- MidTermNetCore/Models/KetQua.cs
+++ MidTermNetCore/Models/KetQua.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace MidTermNetCore.Models
 {
     public class KetQua
     {
+        [Key]
+        [Required]
+        public int MaKetQua { get; set; }
+        public double DiemGK { get; set; }
+        public double DiemCK { get; set; }
+        public virtual SinhVien SinhVienNavigation { get; set; }
+        public virtual LopHocPhan LopHocPhanNavigation { get; set; }
     }
 }
