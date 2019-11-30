99aeed237c8d3bd208b8b1eb6f38fdad324d3975
HoangSonLe <hoangsonle211@gmail.com>
2019-11-30 08:35:39 +0700

    init project

---
+++ MidTermNetCore/Models/KetQua.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MidTermNetCore.Models
+{
+    public class KetQua
+    {
+    }
+}
---
+++ MidTermNetCore/Models/SinhVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MidTermNetCore.Models
+{
+    public class SinhVien
+    {
+        [Key]
+        [Required]
+        public string MaSV { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string DienThoai { get; set; }
+        public virtual string MaKhoaNavigation { get; set; }
+        public virtual ICollection<KetQua> KetQua { get; set; }
+    }
+}
